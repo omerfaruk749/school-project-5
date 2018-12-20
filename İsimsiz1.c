410dd759df729816d2e26add8f906986e9781426
omerfaruk749 <45922939+omerfaruk749@users.noreply.github.com>
2018-12-20 13:26:31 +0300

    Create İsimsiz1.c

---
+++ İsimsiz1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,c,d,istek;
+	printf("Kac satiri goruntulemek istesiniz? ");
+	scanf("%d\n",&istek);
+	for(a=0;a<istek;a++)
+	{
+		for (b=0;b<=c;b++)
+ 		{
+ 			printf("%d",c);
+ 			c=c*(b-e)/(e+1);
+		}
+	}
+	
+	
+}
